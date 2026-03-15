7719ae416fb70514f06d9e696f4cb7597d8581a4
Alex Motor <boldwebdevelopment@gmail.com>
2026-03-15 13:46:28 -0700

    Updates

---
+++ _next/static/V1j_q31IMxVVaTN0fF-FY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/996-80bd74c565659df8.js","static/chunks/pages/index-4a4447d001bfaaa6.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
